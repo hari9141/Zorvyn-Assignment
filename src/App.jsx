bf6fc8f1b6f561f0fd6d34e8072de2fe04cb9fdd
hari9141 <138037811+hari9141@users.noreply.github.com>
2026-04-06 21:13:46 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,7 @@
+import React from 'react'
+import FinanceProvider from './context/FinanceContext'
+import Dashboard from './components/Dashboard'
 
+export default function App() {
+  return <FinanceProvider><Dashboard /></FinanceProvider>
+}
